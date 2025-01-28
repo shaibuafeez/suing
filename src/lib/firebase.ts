820ace7a2b94163582d951799777c76d3614f8ea
afeez shaibu <shaibuafeez@gmail.com>
2025-01-28 17:27:46 +0100

    refactor: move Firebase config to environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB_dNjCChtM9TPUIrO9KzMCyWdeEE2XsEg",
-  authDomain: "suing-e7d25.firebaseapp.com",
-  projectId: "suing-e7d25",
-  storageBucket: "suing-e7d25.firebasestorage.app",
-  messagingSenderId: "766499183376",
-  appId: "1:766499183376:web:67da031ebe75b7e8e76395",
-  measurementId: "G-RV6PSJ8066"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
